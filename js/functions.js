78f7a6457c17fa686053497a3fab0197b2e439a3
Morvana Bonin <morvanabonin@gmail.com>
2013-07-23 02:58:43 -0300

    Ajustes finais, falta corrigir o bug do menu.

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,22 @@
-$(document).ready(function(){
-	$('.nav a').click(function(e){
-		$.scrollTo( this.hash || 0, 500);
-		e.preventDefault();
+!function ($){
+	
+    "use strict";
+
+    $(document).ready(function(){
+    	
+		$('.nav a').click(function(e){
+			$.scrollTo( this.hash || 0, 500);
+			e.preventDefault();
+		});
+
+		$('#brand').click(function () {
+			$('body,html').animate({ scrollTop: 0}, 800);
+			return false;
+		});
+
+		$('.anchorLink').click(function() {
+        	$('.nav-collapse').collapse('toggle');
+    	});
 	});
-	$('#brand').click(function () {
-		$('body,html').animate({ scrollTop: 0}, 800);
-		return false;
-	});
-});
+
+}(window.jQuery);
