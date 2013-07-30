cf7e703f78d7958f670faee2eb13226c6da246c5
Morvana Bonin <morvanabonin@gmail.com>
2013-07-29 23:06:53 -0300

    Acrescimo de palestrantes e horario do evento.

--- js/functions.js
+++ js/functions.js
@@ -2,21 +2,8 @@
 	
     "use strict";
 
-    $(document).ready(function(){
-    	
-		$('.nav a').click(function(e){
-			$.scrollTo( this.hash || 0, 500);
-			e.preventDefault();
-		});
-
-		$('#brand').click(function () {
-			$('body,html').animate({ scrollTop: 0}, 800);
-			return false;
-		});
-
 		$('.anchorLink').click(function() {
         	$('.nav-collapse').collapse('toggle');
     	});
-	});
 
 }(window.jQuery);
